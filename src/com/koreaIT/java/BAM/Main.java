9904d116fe06bdd01218f9d3b7ec2946bf437f56
IRON0567 <zxcjfgml0567@gmail.com>
2022-12-18 15:22:45 +0900

    게시물 상세페이지 구현중(날짜x)

--- src/com/koreaIT/java/BAM/Main.java
+++ src/com/koreaIT/java/BAM/Main.java
@@ -70,22 +70,27 @@ public class Main {
 				int id = Integer.parseInt(cmdBits[2]);
 				
 				boolean found = false;
+				Article foundArticle = null;
 				for(int i = 0; i < articles.size(); i++) {
 					Article article = articles.get(i);
 					
 					if(article.id == id) {
 						found = true;
+						foundArticle = article;
 						System.out.printf("%d번 게시물은 존재합니다\n", id);
-						
+						break;
 					}
 				}
 				
 				
 				if(found == false) {
-				
 				System.out.printf("%d번 게시물은 존재하지 않습니다\n", id);
-				
-				
+				continue;
+				}else {
+					System.out.printf("번호 : %d\n", foundArticle.id);
+					System.out.printf("날짜 : %d\n", "2022-12-12 12:12:12");
+					System.out.printf("제목 : %d\n", foundArticle.title);
+					System.out.printf("내용 : %d\n", foundArticle.body);
 				}
 				
 			}else {
